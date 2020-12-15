11acbd974c3afaf216ea76c0d37cb90cd4b340cb
quocbaostu <72492204+quocbaostu@users.noreply.github.com>
2020-12-15 22:39:19 +0700

    Update nhiệm vụ

--- single.php
+++ single.php
@@ -1,15 +1,13 @@
+<?php
+	include "./includes/connect.php";
+?>
+
 <!--A Design by W3layouts 
 Author: W3layout
 Author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 -->
-
-<!-- Kết nối DB: -->
-<?php
-	include "./includes/connect.php";
-?>
-
 <!DOCTYPE html>
 <html>
 
@@ -19,16 +17,23 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	<!--theme-style-->
 	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 	<link rel="stylesheet" href="css/etalage.css" type="text/css" media="all" />
+	<link href="css/my.css" rel="stylesheet" type="text/css" media="all" />
 	<!--//theme-style-->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://getbootstrap.com/examples/offcanvas/offcanvas.css" rel="stylesheet">
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
+        crossorigin="anonymous"></script>
 	<script
 		type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<!--fonts-->
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	
 	<!--//fonts-->
 	<script src="js/jquery.min.js"></script>
-
 	<script src="js/jquery.etalage.min.js"></script>
+
 	<script>
 		jQuery(document).ready(function ($) {
 
@@ -46,41 +51,62 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		});
 	</script>
 
+	<style>
+	.rsidebar{
+		margin-left: 12px;
+	}
+
+</style>
+
 </head>
 
 <body>
-	<!-- HEADER -->
-	<div class="header">
-		<?php
-			include "./includes/header.php";
-		?>
-	</div>
-	
-	<!-- CONTENT -->
+	<!--header-->
+	<?php
+		include './includes/header.php';
+	?>
+
+	<!---->
+
 	<div class="container">
 
 		<div class=" single_top">
+		<?php
+			$para=array();
+			//$trang=1;
+			if(isset($_GET['maxe'])){
+				$sql="select * from xe where maxe=?";
+				$para[]=$_GET['maxe'];
+			}else
+				$sql="select * from xe";
+			$stmt=$conn->prepare($sql);
+	
+			$stmt->execute($para);
+			$xe=$stmt->fetch(PDO::FETCH_ASSOC);
+			
+		?>
+
 			<div class="single_grid">
 				<div class="grid images_3_of_2">
 					<ul id="etalage">
 						<li>
 							<a href="optionallink.html">
-								<img class="etalage_thumb_image" src="images/s4.jpg" class="img-responsive" />
-								<img class="etalage_source_image" src="images/si4.jpg" class="img-responsive"
+								<img class="etalage_thumb_image" src="images/pro_img<?php echo explode(',',$xe['hinh'])[0]; ?>" class="img-responsive" />
+								<img class="etalage_source_image" src="images/pro_img<?php echo explode(',',$xe['hinh'])[0]; ?>" class="img-responsive"
 									title="" />
 							</a>
 						</li>
 						<li>
-							<img class="etalage_thumb_image" src="images/s2.jpg" class="img-responsive" />
-							<img class="etalage_source_image" src="images/si2.jpg" class="img-responsive" title="" />
+							<img class="etalage_thumb_image" src="images/lay_img<?php echo explode(',',$xe['hinh'])[1]; ?>" class="img-responsive" />
+							<img class="etalage_source_image" src="images/lay_img/<?php echo explode(',',$xe['hinh'])[1]; ?>" class="img-responsive" title="" />
 						</li>
 						<li>
-							<img class="etalage_thumb_image" src="images/s3.jpg" class="img-responsive" />
-							<img class="etalage_source_image" src="images/si3.jpg" class="img-responsive" />
+							<img class="etalage_thumb_image" src="images/lay_img<?php echo explode(',',$xe['hinh'])[2]; ?>" class="img-responsive" />
+							<img class="etalage_source_image" src="images/lay_img<?php echo explode(',',$xe['hinh'])[2]; ?>" class="img-responsive" />
 						</li>
 						<li>
-							<img class="etalage_thumb_image" src="images/s1.jpg" class="img-responsive" />
-							<img class="etalage_source_image" src="images/si1.jpg" class="img-responsive" />
+							<img class="etalage_thumb_image" src="images/lay_img<?php echo explode(',',$xe['hinh'])[3]; ?>" class="img-responsive" />
+							<img class="etalage_source_image" src="images/lay_img<?php echo explode(',',$xe['hinh'])[3]; ?>" class="img-responsive" />
 						</li>
 					</ul>
 					<div class="clearfix"> </div>
@@ -88,24 +114,25 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="desc1 span_3_of_2">
 
 
-					<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+					<h2><?php echo $xe['tenxe']; ?></h2>
 					<div class="cart-b">
-						<div class="left-n ">$329.58</div>
-						<a class="now-get get-cart-in" href="#">ADD TO CART</a>
+						<div class="left-n "><?php echo number_format($xe['gia']); ?> VND</div>
+						<form action="cart.php?action=add" method="POST" >
+							<input type="text" value="1" name="txtSluong[<?php echo $xe['maxe'] ?>]">
+							<input class="now-get get-cart-in" type="submit" value="ADD TO CART">
+						</form>
+						<!-- <a class="now-get get-cart-in" href="#">ADD TO CART</a> -->
 						<div class="clearfix"></div>
 					</div>
 					<h6>100 items in stock</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-						voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					<!-- <p><?php echo $xe['mota']; ?></p> -->
 					<div class="share">
 						<h5>Share Product :</h5>
 						<ul class="share_nav">
-							<li><a href="#"><img src="images/facebook.png" title="facebook"></a></li>
-							<li><a href="#"><img src="images/twitter.png" title="Twiiter"></a></li>
+							<li><a href="https://www.facebook.com/"><img src="images/facebook.png" title="facebook"></a></li>
+							<li><a href="https://twitter.com/?lang=vi"><img src="images/twitter.png" title="Twiiter"></a></li>
 							<li><a href="#"><img src="images/rss.png" title="Rss"></a></li>
-							<li><a href="#"><img src="images/gpluse.png" title="Google+"></a></li>
+							<li><a href="https://www.google.com/"><img src="images/gpluse.png" title="Google+"></a></li>
 						</ul>
 					</div>
 
@@ -114,35 +141,39 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="clearfix"> </div>
 			</div>
 
-			<!-- Hiển thị sản phẩm cùng loại || sản phẩm radom -->
-			<br>
+			<div class="toogle">
+				<h3 class="m_3">Product Details</h3>
+				<p class="m_text">
+					<?php echo $xe['mota']; ?>
+				</p>
+			</div><br><br>
+
+
+			<?php
+				$maxe=isset($_GET['maxe'])?$_GET['maxe']:'';
+				$maloai=isset($_GET['maloai'])?$_GET['maloai']:'';
+				$paraMa=[$maxe, $maloai,];
+				$sqlLoai = "SELECT * FROM xe WHERE maloai ='$maloai' AND maxe <> '$maxe' ";
+				$st = $conn->prepare($sqlLoai);
+				$st->execute($para);
+				$dataLoai = $st->fetchAll(PDO::FETCH_ASSOC);
+				//var_dump($para);exit;
+			?>
 			<h2>OTHER PRODUCTS</h2>
 			<ul id="flexiselDemo1">
-				<li><img src="images/pi.jpg" />
-					<div class="grid-flex"><a href="#">Bloch</a>
-						<p>Rs 850</p>
+				<?php
+					$sqlRan = "SELECT * FROM xe ORDER BY RAND ( ) LIMIT 5";
+					$stmt = $conn->prepare($sqlRan);
+					$stmt->execute();
+					$dataRan = $stmt->fetchAll(PDO::FETCH_ASSOC);
+					foreach($dataRan as $key=>$value){
+				?>
+				<li><img src="images/pro_img/<?php echo explode(',',$value['hinh'])[0]; ?>" />
+					<div class="grid-flex"><a href="#"><?php echo $value['tenxe']; ?></a>
+						<p><?php echo number_format($value['gia']); ?></p>
 					</div>
 				</li>
-				<li><img src="images/pi1.jpg" />
-					<div class="grid-flex"><a href="#">Capzio</a>
-						<p>Rs 850</p>
-					</div>
-				</li>
-				<li><img src="images/pi2.jpg" />
-					<div class="grid-flex"><a href="#">Zumba</a>
-						<p>Rs 850</p>
-					</div>
-				</li>
-				<li><img src="images/pi3.jpg" />
-					<div class="grid-flex"><a href="#">Bloch</a>
-						<p>Rs 850</p>
-					</div>
-				</li>
-				<li><img src="images/pi4.jpg" />
-					<div class="grid-flex"><a href="#">Capzio</a>
-						<p>Rs 850</p>
-					</div>
-				</li>
+				<?php } ?>
 			</ul>
 			<script type="text/javascript">
 				$(window).load(function () {
@@ -173,33 +204,24 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			</script>
 			<script type="text/javascript" src="js/jquery.flexisel.js"></script>
 
-			<div class="toogle">
-				<h3 class="m_3">Product Details</h3>
-				<p class="m_text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-					euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-					nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
-					autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore
-					eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent
-					luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta
-					nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
-			</div>
 		</div>
 
-		<!-- Menu trái -->
+		<!--Menu Left-->
 		<div class="sub-cate">
 			<?php
-				include "./includes/menuleft.php";
-			?>
+        	include './includes/menuleft.php'
+        	?>
+
+
 		</div>
 		<div class="clearfix"> </div>
 	</div>
 
-	<!-- FOOTER -->
-	<div class="footer">
-		<?php
-			include "./includes/footer.php";
-		?>
-	</div>
+	<!-- FOOTER-->
+	<?php
+		include './includes/footer.php';
+	?>
+
 </body>
 
 </html>
