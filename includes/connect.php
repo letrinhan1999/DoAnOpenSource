1e91508437f510df7f1d00ea5aa7626d85ecb55a
buithanhnguyen <nguyen1k9@gmail.com>
2020-12-15 19:07:04 +0700

    view login logut, addproduct, add admin

--- includes/connect.php
+++ includes/connect.php
@@ -1,3 +1,17 @@
-<!-- CONNECT -->
-
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$xe1trang=6;
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=motorcyclestore", $username, $password);
+  // set the PDO error mode to exception
+  $conn -> query('set names utf8');
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "<div hidden>Connection successfully!!! </div>";
+} catch(PDOException $e) {
+  echo "<div hidden>Connection failed: ". $e->getMessage().'</div>' ;
+}
+?>
